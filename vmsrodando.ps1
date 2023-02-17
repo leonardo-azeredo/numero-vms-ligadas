fc2fc0e0abed4332c0dc5d7a40c895aad58b3625
unknown <leonardoazeredosjc@gmail.com>
2023-02-17 11:50:01 -0300

    final

---
+++ vmsrodando.ps1
@@ -0,0 +1,5 @@
+﻿$vms = Get-AzVM -Status 
+$rodando = $vms | Where-Object { $_.PowerState -eq "VM running" }
+$rodandonome = $rodando | Select-Object -ExpandProperty Name
+$contadorodando = $rodando.Count
+Write-Output "O número de VM ligadas são: $contadorodando " " As máquinas ligadas são:" $rodandonome
